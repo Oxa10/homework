42ec7342bb99fdc81ee720313f13789dd0900b05
Othman Al-rawi <163772732+Oxa10@users.noreply.github.com>
2025-04-14 13:07:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    N += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    N = 0
+})
+input.onButtonPressed(Button.B, function () {
+    N += -1
+})
+let N = 0
+basic.showNumber(0)
+basic.forever(function () {
+	
+})
